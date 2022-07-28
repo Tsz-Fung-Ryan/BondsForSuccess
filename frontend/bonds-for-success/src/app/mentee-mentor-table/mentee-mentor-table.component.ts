e6a5abebaf2b66da41ad8fba236703a3d6b7edf4
Tom Brunton <bruntontom1313@gmail.com>
2022-07-27 21:44:58 -0400

    Added functionality to allow user to rearrange matches

--- frontend/bonds-for-success/src/app/mentee-mentor-table/mentee-mentor-table.component.ts
+++ frontend/bonds-for-success/src/app/mentee-mentor-table/mentee-mentor-table.component.ts
@@ -1,7 +1,7 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
-import { Match } from '../libs/api/generated-code/api';
+import { Match, Person } from '../libs/api/generated-code/api';
 import { MatchedResultsService } from '../services/matched-results.service';
 
 @Component({
@@ -10,11 +10,17 @@ import { MatchedResultsService } from '../services/matched-results.service';
   styleUrls: ['./mentee-mentor-table.component.scss']
 })
 export class MenteeMentorTableComponent implements OnInit {
-  @ViewChild('menteeMentorTable') menteeMentorTable: MatTable<Match> | undefined;
+  @ViewChild('menteeTable') menteeTable: MatTable<Person> | undefined;
+  @ViewChild('mentorTable') mentorTable: MatTable<Person> | undefined;
 
-  public displayedColumns = ['menteeName', 'menteeEmail', 'mentorName', 'mentorEmail'];
-  public displayedHeaderRowColums = ['header-row-mentor', 'header-row-mentee'];
+  public menteeDisplayedColumns = ['menteeName', 'menteeEmail'];
+  public menteeDisplayedHeaderRowColumns = ['header-row-mentee'];
+  public mentorDisplayedColumns = ['mentorName', 'mentorEmail'];
+  public mentorDisplayedHeaderRowColumns = ['header-row-mentor'];
+
   public menteeMentorData: Match[] = [];
+  public menteeData: Person[] = [];
+  public mentorData: Person[] = [];
 
   constructor(
     private matchedResults: MatchedResultsService,
@@ -23,13 +29,52 @@ export class MenteeMentorTableComponent implements OnInit {
   ngOnInit(): void {
     this.matchedResults.matchedResultsChanged.subscribe(matchedResults => {
       this.menteeMentorData = matchedResults;
+      const { menteeData, mentorData } = this.getMenteeAndMentorData(matchedResults);
+
+      this.menteeData = menteeData;
+      this.mentorData = mentorData;
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    if (this.menteeMentorData) {
-      moveItemInArray(this.menteeMentorData, event.previousIndex, event.currentIndex);
-      this.menteeMentorTable?.renderRows();
-    }
+  getMenteeAndMentorData(matchedResults: Match[]): { menteeData: Person[], mentorData: Person[] } {
+    const menteeData: Person[] = [];
+    const mentorData: Person[] = [];
+
+    matchedResults.forEach((matchedResult) => {
+      menteeData.push(matchedResult.mentee);
+      mentorData.push(matchedResult.mentor);
+    });
+
+    return {
+      menteeData: menteeData,
+      mentorData: mentorData,
+    };
+  }
+
+  drop(event: CdkDragDrop<string[]>, arrayToRearrange: Person[]) {
+    moveItemInArray(arrayToRearrange, event.previousIndex, event.currentIndex);
+    this.menteeTable?.renderRows();
+    this.mentorTable?.renderRows();
+  }
+
+  handleDownloadButtonClicked(): void {
+    const combinedMenteeAndMentorData = this.combineMenteeAndMentorData(this.menteeData, this.mentorData);
+
+    console.log(combinedMenteeAndMentorData);
+  }
+
+  combineMenteeAndMentorData(menteeData: Person[], mentorData: Person[]): Match[] {
+    const combinedMatches: Match[] = [];
+
+    menteeData.forEach((mentee, index) => {
+      const newMatch: Match = {
+        mentee: mentee,
+        mentor: mentorData[index],
+      };
+
+      combinedMatches.push(newMatch);
+    });
+
+    return combinedMatches;
   }
 }
